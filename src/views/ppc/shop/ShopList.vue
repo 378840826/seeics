4a604d7ae41362b65df28e3df49cfd0a71994d8c
xjt1206 <378840826@qq.com>
2022-04-22 17:51:02 +0800

    广告授权修改

--- src/views/ppc/shop/ShopList.vue
+++ src/views/ppc/shop/ShopList.vue
@@ -219,7 +219,7 @@ const addAmazonSdk = () => {
   amazonRoot.id = 'amazon-root';
   document.body.appendChild(amazonRoot);
   window.onAmazonLoginReady = function () {
-    window.amazon.Login.setClientId('amzn1.application-oa2-client.2ff7990ca97a467d823cd77832577b90');
+    window.amazon.Login.setClientId('amzn1.application-oa2-client.f5629ca0717645ac8ed861a8408327aa');
   };
   (function (document) {
     const a = document.createElement('script');
